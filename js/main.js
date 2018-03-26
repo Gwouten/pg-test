d5fc718508c3e8f7d3dc7008fc3865be58da74bf
Wouter Deloof <Gwouten@Wouters-iMac-2.local>
2018-03-27 00:00:33 +0200

    Added JS for submit button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const inputs = document.querySelectorAll('input[name="poll__option"]');
+    const submitBtn = document.querySelector('.btn--poll');
+
+    inputs.forEach((item) => {
+        item.parentNode.onchange = () => {
+            submitBtn.disabled = false;
+        }
+    });
+
+
+});
